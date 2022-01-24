39ee87f9c4d0ca0a71445360d2610b5ef6c5a02b
praveen kumar <praveen@vyaparapp.in>
2022-01-24 21:03:59 +0530

    added calculator functionalities

--- src/components/calculator/index.js
+++ src/components/calculator/index.js
@@ -1,32 +1,77 @@
-import React  from "react";
+import React, {useState}  from "react";
 import "./index.css";
 
 export default function Calculator() {
+
+  const [label, setLabel] = useState('');
+  const [totalOps, setTotalOps] = useState(0);
+  const [result,setResult] = useState(0);
+  const [firstValue,setFirstValue] = useState(0);
+  const [secondValue,setSecondValue] = useState(0);
+
+  const changeFirstValue = (event) =>{
+    let value = parseFloat(event.target.value);
+    setFirstValue(value);
+  
+  }
+  const changeSecondValue = (event) =>{
+    let value = parseFloat(event.target.value);
+    setSecondValue(value);
+    
+  }
+
+  const doSum = () =>{
+      setResult(firstValue+secondValue);
+      setTotalOps(totalOps+1);
+      setLabel('+');
+  }
+  const doSub = () =>{
+    setResult(firstValue-secondValue);
+    setTotalOps(totalOps+1);
+    setLabel('-');
+  }
+  const doMul = () =>{
+    setResult(firstValue*secondValue);
+    setTotalOps(totalOps+1);
+    setLabel('*');
+  }
+  const doDiv = () =>{
+    setResult(firstValue/secondValue);
+    setTotalOps(totalOps+1);
+    setLabel('/');
+  }
+  const resetData = () =>{
+    setFirstValue(0);
+    setSecondValue(0);
+    setTotalOps(0);
+    setLabel('');
+    setResult(0);
+  }
   return (
     <div className="layout-column align-items-center">
-      <div data-testid="total-operations" className="pt-50 total-operations"></div>
+      <div data-testid="total-operations" className="pt-50 total-operations">Total Operation Performed : {totalOps}</div>
       <div className="card">
 
         <section className="card-text">
           <div className="layout-row justify-content-around align-items-center mt-40">
-            <input type="number" className="ml-3 mr-3" data-testid="app-input1" autoComplete="off" placeholder="Eg: 1"
+            <input type="number" onChange={changeFirstValue} value={firstValue} className="ml-3 mr-3" data-testid="app-input1" autoComplete="off" placeholder="Eg: 1"
                   name="input1"/>
-            <label className="ml-2 mr-2 symbol text-center" data-testid="selected-operator"></label>
-            <input type="number" data-testid="app-input2" autoComplete="off" className="ml-3 mr-3"
+            <label className="ml-2 mr-2 symbol text-center" data-testid="selected-operator">{label}</label>
+            <input type="number" onChange={changeSecondValue} value={secondValue} data-testid="app-input2" autoComplete="off" className="ml-3 mr-3"
                   placeholder="Eg: 2"/>
           </div>
 
           <div className="layout-row justify-content-around mt-30">
-            <button className="operationFont" type="submit" data-testid="addButton">+</button>
-            <button className="operationFont" type="submit" data-testid="subtractButton">-</button>
-            <button className="operationFont" type="submit" data-testid="multiplyButton">*</button>
-            <button className="operationFont" type="submit" data-testid="divideButton">/</button>
+            <button className="operationFont" onClick={doSum} type="submit" data-testid="addButton">+</button>
+            <button className="operationFont" onClick={doSub} type="submit" data-testid="subtractButton">-</button>
+            <button className="operationFont" onClick={doMul} type="submit" data-testid="multiplyButton">*</button>
+            <button className="operationFont" onClick={doDiv} type="submit" data-testid="divideButton">/</button>
           </div>
 
           <div className="layout-row justify-content-between align-items-center mt-30">
-            <button type="reset" data-testid="resetButton" className="outline danger">Reset</button>
+            <button type="reset" onClick={resetData} data-testid="resetButton" className="outline danger">Reset</button>
             <div className="layout-row justify-content-center align-items-center result-container">
-              <div data-testid="result" className="result-value ma-0 slide-up-fade-in"></div>
+              <div data-testid="result" className="result-value ma-0 slide-up-fade-in">Result : {result}</div>
             </div>
           </div>
         </section>
